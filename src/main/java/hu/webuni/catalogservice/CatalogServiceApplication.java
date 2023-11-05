c75e79a3de83b547b3c3708212eb447de0bb2997
zsoli <jakabzsoli89@gmail.com>
2023-11-05 16:04:01 +0100

    feat: refactor catalog-service

--- src/main/java/hu/webuni/catalogservice/CatalogServiceApplication.java
+++ src/main/java/hu/webuni/catalogservice/CatalogServiceApplication.java
@@ -1,6 +1,7 @@
 package hu.webuni.catalogservice;
 
 import hu.webuni.catalogservice.service.InitDbService;
+import hu.webuni.security.JwtAuthFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackageClasses = {CatalogServiceApplication.class, JwtAuthFilter.class})
 @RequiredArgsConstructor
 @EnableCaching
 public class CatalogServiceApplication implements CommandLineRunner {
---
+++ src/main/java/hu/webuni/catalogservice/service/SecurityConfig.java
@@ -0,0 +1,45 @@
+package hu.webuni.catalogservice.service;
+
+import hu.webuni.security.JwtAuthFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    @Autowired
+    JwtAuthFilter jwtAuthFilter;
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf ->
+                        csrf.disable()
+                )
+                .sessionManagement(sessionConfig ->
+                        sessionConfig.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .requestMatchers(HttpMethod.GET, "/api/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/api/**").hasAuthority("admin")
+                                .requestMatchers(HttpMethod.PUT, "/api/**").hasAuthority("admin")
+                                .requestMatchers(HttpMethod.DELETE, "/api/**").hasAuthority("admin")
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build()
+                ;
+    }
+
+}
